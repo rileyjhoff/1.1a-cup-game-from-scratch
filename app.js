9487a508a2763d576eec5f6cbe829ab47979bbc6
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-08 11:15:39 -0800

    grabbed DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // import functions and grab DOM elements
+const leftContainer = document.getElementById('left-cup-container');
+const centerContainer = document.getElementById('center-cup-container');
+const rightContainer = document.getElementById('right-cup-container');
+
+const leftButton = document.getElementById('left-button');
+const centerButton = document.getElementById('center-button');
+const rightButton = document.getElementById('right-button');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+console.log(leftContainer, centerContainer, rightContainer);
+console.log(leftButton, centerButton, rightButton);
+console.log(winsEl, lossesEl, totalEl);
 
 // let state
 
